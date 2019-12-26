ddacaa7f2a4cf99f9aeccb1792a791a7338e8d68
K e n V <kenneth_valdivia@yahoo.com>
2019-12-26 03:17:54 +0000

    Automatically backed up by Learn

---
+++ lib/square_array.rb
@@ -0,0 +1,14 @@
+public static void main(String args[]) throws IOException 
+    { 
+        int ar[] = { 1, 2, 3, 4, 5, 6, 7, 8 }; 
+        int i, x; 
+  
+        // iterating over an array 
+        for (i = 0; i < ar.length; i++) { 
+  
+            // accessing each element of array 
+            x = ar[i]; 
+            System.out.print(x + " "); 
+        } 
+    } 
+} 
